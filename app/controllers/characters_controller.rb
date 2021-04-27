a1db35f15751341f3f111a5e4cb496da16208009
NickyEXE <dovern42@gmail.com>
2021-04-27 19:53:00 -0400

    end of lecture

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,9 +1,29 @@
 class CharactersController < ApplicationController
 
 
+  # Read - Index
   get "/characters" do
+    @characters = Character.all
     erb :"characters/index"
   end
+
+  # Show
+
+  get "/characters/:id" do
+    # whatever's put in the id section gets added to params at a key of id
+    @character = Character.find(params[:id])
+    erb :"characters/show"
+  end
+
+  # New
+
+  # Create
+
+  # Edit
+
+  # Update
+
+  # Delete
 
   get "/cheese" do
     erb :"characters/cheese"
